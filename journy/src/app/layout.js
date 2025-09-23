e73a48af10dcd30fa30cf94df6ffdac477b36c1c
Twahaaa <abubakkertwaha@gmail.com>
2025-09-23 12:08:38 +0530

    second commit

--- journy/src/app/layout.js
+++ journy/src/app/layout.js
@@ -1,5 +1,9 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+// import { Toaster } from "@/components/ui/sonner";
+import { ClerkProvider } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
+// import SidebarWrapper from "../components/SidebarWrapper"; 
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -18,12 +22,18 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
+    <ClerkProvider
+      appearance={{
+        baseTheme: [dark],
+      }}
+    >
+      <html lang="en">
+        <body
+          className={`${geistSans.variable} ${geistMono.variable} antialiased lg:flex bg-black min-h-screen`}
+        >
+          <main className="flex-1">{children}</main>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
